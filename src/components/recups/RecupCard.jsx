7f018b65c94998592090c6820a0e830c6c4dadb0
clara seijo <clasei@proton.me>
2024-10-20 22:50:01 +0200

    totalSaved added and working, yay

--- src/components/recups/RecupCard.jsx
+++ src/components/recups/RecupCard.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import service from "../../services/config"
 
 import { howManyDaysAgo } from "../../utils/dateUtils"
+import { tokenPayload } from '../../utils/token'
 
 function RecupCard({ recommendation }) {
 
+  const token = localStorage.getItem("authToken")
+  const currentUser = tokenPayload(token)._id
+
   // limit textRec
   const getExcerpt = (text) => {
-    return text.length > 120 ? text.substring(0, 120) + '...' : text;
-  };
+    return text.length > 120 ? text.substring(0, 120) + '...' : text
+  }
+
+    // save the recup
+    const handleSaveRecup = async () => {
+      try {
+        const response = await service.put(`/users/${currentUser}/save/${recommendation._id}`)
+        console.log(response.data.message);
+      } catch (error) {
+        console.error("Error saving recup:", error.response?.data?.message || error.message)
+      }
+    }
 
   return (
     <div className="card mb-3 shadow-sm">
@@ -21,6 +36,13 @@ function RecupCard({ recommendation }) {
         <Link to={`/recommendations/detail/${recommendation._id}`} className="btn">
           check this recup
         </Link>
+
+        {/* Check if the logged-in user is not the creator of this recup */}
+        {currentUser !== recommendation.creator._id && (
+          <button className="btn btn-save mt-2" onClick={handleSaveRecup}>
+            Save Recup
+          </button>
+        )}
   
         <p className="days-ago text-muted text-center" style={{ fontSize: '0.7rem' }}>
               {recommendation.createdAt
