ac73d1a8bbc26ed5bb8f274f3499169edabef124
clara seijo <clasei@proton.me>
2024-10-21 11:08:15 +0200

    footer updated

--- src/components/users/UserProfile.jsx
+++ src/components/users/UserProfile.jsx
@@ -1,21 +1,9 @@
-// import { useState, useEffect } from "react"
-// import { useParams, useNavigate } from "react-router-dom"
-// import service from "../../services/config"
-// import RecommendationCard from "../recups/RecupCard"
-// import { tokenPayload } from "../../utils/token" 
+import React from 'react'
 
-// function UserProfile() {
-//   const { username } = useParams()
-//   const navigate = useNavigate()
+function UserProfile() {
+  return (
+    <div>UserProfile</div>
+  )
+}
 
-//   const token = localStorage.getItem("authToken")
-//   const currentUser = token ? tokenPayload(token).username : null
-
-
-//   const [userData, setUserData] = useState(null)
-//   const [errorMessage, setErrorMessage] = useState("")
-
-//     return 
-// }
-
-// export default UserProfile
+export default UserProfile
