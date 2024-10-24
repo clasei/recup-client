3fbca87ed8df9289637a8242142e75a01f9a48fc
clara seijo <clasei@proton.me>
2024-10-24 09:03:27 +0200

    bug to auto-check profile kind of solved

--- src/components/users/UserProfile.jsx
+++ src/components/users/UserProfile.jsx
@@ -1,29 +1,69 @@
-import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useState, useEffect, useContext } from "react"
+import { useParams, useNavigate } from "react-router-dom"
 import service from "../../services/config"
 import RecupCard from "../recups/RecupCard"
 import PropagateLoader from "react-spinners/PropagateLoader"
+import { AuthContext } from "../../context/auth.context"
 
 
 function UserProfile({ setSavedRecs, savedRecs }) {
   const { username } = useParams()
   const [userRecups, setUserRecups] = useState([])
   const [loading, setLoading] = useState(true)
+  const navigate = useNavigate()
+  const { loggedUserId } = useContext(AuthContext)
+  const [loggedUsername, setLoggedUsername] = useState("")
 
   useEffect(() => {
-    const fetchUserRecups = async () => {
+    const fetchLoggedUsername = async () => {
       try {
-        const response = await service.get(`/users/created/${username}`)
-        setUserRecups(response.data) 
+        const response = await service.get(`/users/user-profile/${loggedUserId}`)
+        setLoggedUsername(response.data.username)
+        console.log(response.data.username)
       } catch (error) {
-        console.error("Error fetching user recups:", error)
-      } finally {
-        setLoading(false)
+        console.error("Error fetching logged user data:", error);
       }
-    }
+    };
 
-    fetchUserRecups()
-  }, [username])
+    if (loggedUserId) {
+      fetchLoggedUsername()
+    }
+  }, [loggedUserId])
+
+  // useEffect(() => {
+  //   const fetchUserRecups = async () => {
+  //     try {
+  //       const response = await service.get(`/users/created/${username}`)
+  //       setUserRecups(response.data) 
+  //     } catch (error) {
+  //       console.error("Error fetching user recups:", error)
+  //     } finally {
+  //       setLoading(false)
+  //     }
+  //   }
+
+  //   fetchUserRecups()
+  // }, [username])
+
+  useEffect(() => {
+    if (loggedUsername, loggedUsername === username) {
+      alert("hey you look pretty fine, here you can check your own recups")
+      navigate(`/created`)
+    } else {
+
+      const fetchUserRecups = async () => {
+        try {
+          const response = await service.get(`/users/created/${username}`);
+          setUserRecups(response.data);
+        } catch (error) {
+          console.error("Error fetching user recups:", error);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchUserRecups();
+    }
+  }, [username, loggedUsername, navigate, loggedUserId])
 
   if (loading) {
     return (
